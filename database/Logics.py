e7c3f919c19648ade22226633c251b1b3e1d53af
Rodrigo Salazar <rods.273@gmail.com>
2020-07-14 19:21:32 -0600

    Modificar usuario desarrollado
    update usaurio mepezado

--- database/Logics.py
+++ database/Logics.py
@@ -429,4 +429,27 @@ class adminOpciones(DatabaseZ):
             lista.append(dicc)
         return lista
 
+class adminCuenta():
+    def __init__(self):
+        self.database = DatabaseZ()
 
+    def updateusuario(self,datanueva):
+        """ actualiza los campos de la cuenta de un usuario recibiendo un diccionario con los nuevos campos y el id"""
+        database = self.database
+        sql = """UPDATE hermes.clientes SET
+            DUI=%s , Nombre=%s, Apellido=%s, Celular=%s, Direccion=%s, Correo=%s ,
+            Contrasena=%s , Departamento=%s , Municipio=%s WHERE idClientes=%s;"""
+        val = (
+            datanueva['dui'],
+             datanueva['nombre'],
+             datanueva['apellido'],
+            datanueva['telefono'],
+            datanueva['direccion'],
+            datanueva['correo'],
+            datanueva['contra'],
+            datanueva['departamento'],
+            datanueva['municipio'],
+            datanueva['id']
+            )
+        success = database.executeMany(sql,val)
+        return success
